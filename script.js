db10d0fe0ad85d86afa0e783a22941cc4e4dd2f0
Trist-GitHub <57120755+Trist-GitHub@users.noreply.github.com>
2019-11-15 13:00:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) 
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+app.use(cors());
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+app.get('/download', (req,res) => {
+    var URL = req.query.URL;
+    res.json({url:URL});
+})
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) {
+    fetch(`http://localhost:4000/download?URL=${URL}`, {
+        method:'GET'
+    }).then(res => res.json())
+    .then(json => console.log(json));
+}
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+app.use(cors());
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+app.get('/download', (req,res) => {
+var URL = req.query.URL;
+res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+ytdl(URL, {
+    format: 'mp4'
+    }).pipe(res);
+});
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) {
+    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+}
+});
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) {
+    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+}
